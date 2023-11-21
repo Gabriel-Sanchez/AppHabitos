0c73fc4d078335d08b3b46944bde1af898770853
Gabriel-Sanchez <gabriel.g.sanchez.c@gmail.com>
2023-11-20 22:10:07 -0500

    scroll

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ import json
 
 import ctypes
 
+
+
+from PIL import Image, ImageTk
+
+# Crea una nueva ventana
+
+
+def on_mousewheel(event):
+    shift = (event.state & 0x1) != 0
+    scroll = -1 if event.delta > 0 else 1
+    if shift:
+        canvas.xview_scroll(scroll, "units")
+    else:
+        canvas.yview_scroll(scroll, "units")
+
+
+
 # Función para ocultar botones de minimizar y cerrar
 def ocultar_botones():
     # Obtener el identificador de la ventana principal
@@ -71,34 +88,75 @@ with open('habitos/lista_habitos.json', 'r') as f:
 
 
 # Crear diccionario de opciones con nombres como claves e IDs como valores
-opciones = {item['nombre']: item['id'] for item in data}
+opciones = {item['nombre']: { 'id': item['id'], 'type': item['type']  }    for item in data}
+print(opciones)
 
 # Función para mostrar el ID cuando se presiona un botón
 def mostrar_id(nombre):
-    id_seleccionado = opciones[nombre]
+    id_seleccionado = opciones[nombre]['id']
+    print(opciones[nombre]['type'])
     print(f"Seleccionado: {nombre} (ID: {id_seleccionado})")
-    ventana_pomodoro.ventana_pomodoro(id_seleccionado)
+    
+    if opciones[nombre]['type'] == 1:
+        ventana_pomodoro.ventana_pomodoro(id_seleccionado)
+    elif opciones[nombre]['type'] == 2:
+        pass #otro tipo guardar directamente como hecho en el dia en un nuevo csv 
+        
+    
     
 def abrir_calendario(nombre):
-    id_seleccionado = opciones[nombre]
+    id_seleccionado = opciones[nombre]['id']
     print(f"Seleccionado22: {nombre} (ID: {id_seleccionado})")
     habito_calendario(id_seleccionado)
 
 root = tk.Tk()
 
-lista_habitos = tk.Frame(root, bg='white', relief='raised')
-lista_habitos.pack()
+image = Image.open('assets/icon/icon.jpg')
+
+# Convierte la imagen a formato PhotoImage
+photo = ImageTk.PhotoImage(image)
+
+root.wm_iconphoto(True, photo)
+
+
+# lista_habitos = tk.Frame(root, bg='white', relief='raised')
+# lista_habitos.pack()
+
+
+canvas = tk.Canvas(root)
+scrollbar = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
+
+# Crea un frame dentro del canvas
+lista_habitos = tk.Frame(canvas)
+
+# Configura el canvas para que se desplace con la barra de desplazamiento
+canvas.configure(yscrollcommand=scrollbar.set)
+
+# Empaqueta el canvas y la barra de desplazamiento
+canvas.pack(side="left", fill="both", expand=True)
+scrollbar.pack(side="right", fill="y")
+
+# Agrega el frame al canvas
+canvas.create_window((0,0), window=lista_habitos, anchor="nw")
+canvas.bind_all("<MouseWheel>", on_mousewheel)
+
+
+# Actualiza el scrollregion después de que se haya dibujado el frame
+lista_habitos.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+
+
 i = 0
 # Crear botones para cada opción en el diccionario
 for nombre in opciones.keys():
     label_mover = tk.Label(lista_habitos, text=nombre, bg='#089aff', fg='white')
-    label_mover.grid(row=i, column=0, sticky="w")
+    label_mover.grid(row=i, column=0, sticky="w", padx=(100,20))
     
     boton = tk.Button(lista_habitos, text="\u23F0", command=lambda n=nombre: mostrar_id(n))
-    boton.grid(row=i, column=1)
+    boton.grid(row=i, column=1, sticky="w", padx=(50,1))
     
     boton_cal = tk.Button(lista_habitos, text="\u25B2", command=lambda n=nombre: abrir_calendario(n))
-    boton_cal.grid(row=i, column=2)
+    boton_cal.grid(row=i, column=2, sticky="w", padx=(1,100))
     i = i + 1
     print(i)
 
@@ -204,7 +262,6 @@ center_window(root, 500, 400)
 # # Botón para cerrar la ventana
 # btn_cerrar = tk.Button(root, text="Cerrar", command=cerrar_ventana)
 # btn_cerrar.pack()
-
 
 
 
